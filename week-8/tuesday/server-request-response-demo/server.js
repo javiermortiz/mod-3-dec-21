3738ef6566e3cb619d93d71147ec86763e93a87f
Javier Ortiz <javier.ortiz989@gmail.com>
2022-02-08 10:31:42 -0800

    assets

--- week-8/tuesday/server-request-response-demo/server.js
+++ week-8/tuesday/server-request-response-demo/server.js
@@ -1,10 +1,26 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(`${req.method} ${req.url}`);
-    res.statusCode = 200;
+    if (req.method === "GET" && req.url === "/") {
+        const htmlPage = fs.readFileSync("index.html", "utf-8");
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        return res.end(htmlPage);
+    }
+
+    if (req.method === "GET" && req.url === "/main.css") {
+        const cssFile = fs.readFileSync("main.css", "utf-8");
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/css");
+        return res.end(cssFile);
+    }
+    
+    res.statusCode = 404;
     res.setHeader("Content-Type", "text/html");
-    res.end('<h1>Hello world!</h1>');
+    res.end('<h1>Can\'t find that page</h1>');
+
 });
 
 const port = 5000;
